e2dd7300f2ad7eb63f0bbb3be5c6c8773a2e246d
methammer <methammer@gmail.com>
2026-03-11 13:32:20 +0100

    fix: correct empty URL test expectation for normalizeSpotifyURL

--- app_test.go
+++ app_test.go
@@ -13,7 +13,7 @@ func TestNormalizeSpotifyURL(t *testing.T) {
 		{"?si= supprimé",               "https://open.spotify.com/track/4ozKsccDY52n3IEwbBJx0l?si=abc123",     "https://open.spotify.com/track/4ozKsccDY52n3IEwbBJx0l"},
 		{"intl-fr + ?si= ensemble",     "https://open.spotify.com/intl-fr/album/4ozKsccDY52n3IEwbBJx0l?si=xyz","https://open.spotify.com/album/4ozKsccDY52n3IEwbBJx0l"},
 		{"playlist inchangée",          "https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M",            "https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M"},
-		{"URL vide retournée telle quelle", "",                                                                  ""},
+		{"URL vide retournée telle quelle", "", "/"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
